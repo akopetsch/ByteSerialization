373b5b74daf741eba7eb10ab2335f78ee8409a77
Alexander Kopetsch <akopetsch92@gmail.com>
2024-06-04 14:31:59 +0200

    HexStringConverter: implement grouping

--- ByteSerialization.Tests/Unit/IO/HexStringConverterTest.cs
+++ ByteSerialization.Tests/Unit/IO/HexStringConverterTest.cs
@@ -8,6 +8,7 @@ namespace ByteSerialization.Tests.Unit.IO
     public class HexStringConverterTest
     {
         private const string hexString = "deadbeef";
+        private const string hexString_groupSize2 = "dead beef";
         private static readonly byte[] byteArray = [0xDE, 0xAD, 0xBE, 0xEF];
 
         [Fact]
@@ -23,5 +24,11 @@ namespace ByteSerialization.Tests.Unit.IO
             Assert.Equal(
                 expected: hexString, 
                 actual: HexStringConverter.ToHexString(byteArray));
+
+        [Fact]
+        public void Test_ToHexString_groupSize2() =>
+            Assert.Equal(
+                expected: hexString_groupSize2,
+                actual: HexStringConverter.ToHexString(byteArray, groupSize: 2));
     }
 }
--- ByteSerialization/IO/HexStringConverter.cs
+++ ByteSerialization/IO/HexStringConverter.cs
@@ -7,9 +7,11 @@ namespace ByteSerialization.IO
 {
     public static class HexStringConverter
     {
+        private const string Space = " ";
+
         public static byte[] ToByteArray(string hexString)
         {
-            string s = hexString.Replace(" ", string.Empty);
+            string s = hexString.Replace(Space, string.Empty);
             byte[] byteArray = new byte[s.Length / 2];
             for (int i = 0; i < byteArray.Length; i++)
                 byteArray[i] = Convert.ToByte(s.Substring(i * 2, 2), 16);
@@ -27,11 +29,17 @@ namespace ByteSerialization.IO
                 "0" + s : s;
         }
 
-        public static string ToHexString(this byte[] bytes)
+        public static string ToHexString(this byte[] bytes, int? groupSize = null)
         {
             var result = new StringBuilder(bytes.Length * 2);
             for (int i = 0; i < bytes.Length; i++)
+            {
+                // group?
+                if (i != 0 && groupSize.HasValue && i % groupSize.Value == 0)
+                    result.Append(Space);
+
                 result.Append(bytes[i].ToString("x2"));
+            }
             return result.ToString();
         }
     }
