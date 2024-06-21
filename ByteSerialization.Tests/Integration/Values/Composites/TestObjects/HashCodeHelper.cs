709a1771858c1824f87d003a0f73a4a6f4f1616c
Alexander Kopetsch <akopetsch92@gmail.com>
2024-06-21 15:45:45 +0200

    refactor HashCodeHelper

---
+++ ByteSerialization.Tests/Integration/Values/Composites/TestObjects/HashCodeHelper.cs
@@ -0,0 +1,16 @@
+﻿// SPDX-License-Identifier: MIT
+
+namespace ByteSerialization.IO
+{
+    internal static class HashCodeHelper
+    {
+        internal static int CombineHashCodes<T>(params T[] objects)
+        {
+            var hashCode = new HashCode();
+            if (objects != null)
+                foreach (T obj in objects)
+                    hashCode.Add(obj);
+            return hashCode.ToHashCode();
+        }
+    }
+}
